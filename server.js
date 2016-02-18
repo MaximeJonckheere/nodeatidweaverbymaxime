101c7506ca670e2437744cce7904872a8d99798d
maximejonckheere <maximejonckheere@me.com>
2016-02-18 12:35:27 +0000

    Changed message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res, next){
-    res.send("Node at IdWeaver by Maxime");
+    res.send("Node at IdWeaver by Maxime!!!");
 });
 
 var port = process.env.PORT || 3000;
