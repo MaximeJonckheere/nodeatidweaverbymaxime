6f622f9a8998eecca1c4905728b3027a8080b76b
maximejonckheere <maximejonckheere@me.com>
2016-02-25 12:22:33 +0000

    Fixed #1

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,10 +1,26 @@
 var mocha = require("mocha");
 var chai = require("chai");
+var supertest = require("supertest");
 
 var expect = chai.expect;
 
-describe("When I run my first test", function(){
-    it("should expect true", function() {
-        expect(true).equal(true);
+
+
+describe("When I start my server", function(){
+    var app = require("../server");
+    var request = supertest.agent(app.listen());
+    
+    it("should return status 200", function(done) {
+        request
+            .get("/")
+            .expect(200)
+            .end(done);
+    });
+    
+    it("should return 'Node at IdWeaver by Maxime!!!'", function(done) {
+        request
+            .get("/")
+            .expect("Node at IdWeaver by Maxime!!!")
+            .end(done);
     });
 });
